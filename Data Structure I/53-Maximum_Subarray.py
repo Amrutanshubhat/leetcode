566ce62ac4186a97280b12ad7e0b8dde93535ce4
Amrutanshu Bhat <amrubht11@gmail.com>
2022-10-09 21:13:03 +0530

    Added two more solutions to #53

--- Data Structure I/53-Maximum_Subarray.py
+++ Data Structure I/53-Maximum_Subarray.py
@@ -26,6 +26,7 @@ Constraints:
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104
 '''
+#Solution 1 : Time Complexity: O(n^2)
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         arr_len = len(nums)
@@ -39,3 +40,34 @@ class Solution:
             if max_sum<loc_sum:
                     max_sum=loc_sum
         return max_sum
+
+#Solution 2 : Time Complexity: O(nlogn)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        l = 0
+        u = len(nums)-1
+        if(l>u):
+            return -99999999999999999
+        if(l==u):
+            return nums[l]
+        m = (u+1)//2
+        lmax = sum_ = nums[m-1]
+        for i in range(m-2,l-1,-1):
+            sum_+=nums[i]
+            lmax=max(lmax,sum_)
+        rmax = sum_ = nums[m]
+        for i in range(m+1,u+1):
+            sum_+=nums[i]
+            rmax=max(rmax,sum_)
+        return max(lmax+rmax,self.maxSubArray(nums[:m]),self.maxSubArray(nums[m:]))
+
+#Solution 3: Time Complexity: O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxhere = 0
+        max_=nums[0]
+        n = len(nums)
+        for i in range(n):
+            maxhere = max(maxhere+nums[i],nums[i])
+            max_ = max(max_, maxhere)
+        return max_
