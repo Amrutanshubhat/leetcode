3ac66f1d58e96d8f4a359eaa11c2e7a595c42455
Amrutanshu <amrubht11@gmail.com>
2024-11-02 12:00:53 +0530

    T:O(n) S:O(1)

---
+++ daily_challenge/2490_circular_senetence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+		if (sentence.front() != sentence.back()) return false;
+		for (size_t i=0; i<sentence.size(); i++) {
+			if (sentence[i] == ' ')	 {
+				if (sentence[i-1] != sentence[i+1]) return false;
+			}
+		}
+		return true;
+    }
+};
